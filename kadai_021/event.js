febd88a30df839e3465a1dbcd376d6631534f31f
Shimizu-Erika <eri8170@yahoo.co.jp>
2024-07-19 06:06:20 +0900

    課題提出

--- kadai_021/event.js
+++ kadai_021/event.js
@@ -2,6 +2,8 @@ const btn = document.getElementById('btn');
 const text = document.getElementById('text');
 
 btn.addEventListener('click', () => {
+
+  console.log('クリック済');
     
   setTimeout(() => {
     text.textContent = "ボタンをクリックしました";
